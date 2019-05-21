40d75f9cf7c05de60e0b215d10142e751ce16a41
msliang <liangmichelles@gmail.com>
2019-05-21 12:38:05 -0400

    implementing qna maker into bot framework

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from sys import exit
-
+import http.client, urllib.parse, json, time, sys
 class EchoBot():
     async def on_turn(self, context):
         # Check to see if this activity is an incoming message.
@@ -12,4 +12,38 @@ class EchoBot():
                 exit(0)
             else:
                 # Echo the message text back to the user.
-                await context.send_activity(f'I heard you say {context.activity.text} ' )
+                await context.send_activity(f'I heard you say {context.activity.text}')
+
+    async def qnaCaller (self, context):
+
+            host = "qnamakerbotproject-ml.azurewebsites.net";
+            # Authorization endpoint key
+            # From Publish Page
+            endpoint_key = <INSERT KEY> ;
+
+            # Management APIs postpend the version to the route
+            # Part of HOST is prepended to route to work with http library
+            route = "/qnamaker/knowledgebases/19dd10f9-a648-4f0c-bfc7-b4cb617090f5/generateAnswer";
+
+            # JSON format for passing question to service
+            question = "{'question':'{" + context.activity.text + "}'}"
+            #"{'question': {context.activity.text}}"
+            headers = {
+                'Authorization': 'EndpointKey ' + endpoint_key,
+                'Content-Type': 'application/json'
+              }
+
+            conn = http.client.HTTPSConnection(host,port=443)
+            try:
+
+                conn.request ("POST", route, question, headers)
+
+                response = conn.getresponse ()
+
+                answer = response.read ()
+
+                print(json.dumps(json.loads(answer), indent=4))
+
+            except :
+                print ("Unexpected error:", sys.exc_info()[0])
+                print ("Unexpected error:", sys.exc_info()[1])
--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+#import asyncio
 import asyncio
 from botbuilder.core import TurnContext, ConversationState, UserState, MemoryStorage
 from botbuilder.schema import ActivityTypes
+
 from adapter import ConsoleAdapter
 from bot import EchoBot
+
 # Create adapter
 adapter = ConsoleAdapter()
 bot = EchoBot()
-#
+
 loop = asyncio.get_event_loop()
 
 if __name__ == "__main__":
     try:
         # Greet user
-        print("Hi... I'm an echobot. Whatever you say I'll echo back.")
+        print("Hi... I'm an echobot. Ask me a question")
 
-        loop.run_until_complete(adapter.process_activity(bot.on_turn))
+        loop.run_until_complete(adapter.process_activity(bot.qnaCaller))
     except KeyboardInterrupt:
         pass
     finally:
